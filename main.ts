7db79abb73b545440d696178a62d99e1ec1ec7ae
Shakeel Mohamed <smohamed@splunk.com>
2018-06-21 19:15:33 -0700

    Check in initial implementation

---
+++ main.ts
@@ -0,0 +1,53 @@
+class FlippyFlop {
+    _data: Array<number>;
+    constructor() {
+        this._data = [];
+    }
+
+    count() {
+        return this._data.length;
+    }
+
+    _randomInt(max: number) {
+        if (!max || max < 0) {
+            max = this.count();
+        }
+        return Math.floor(Math.random() * (max + 1));
+    }
+
+    flip(element: number) {
+        var success = this._randomInt(1) > 0;
+        if (success) {
+            var position = this._randomInt(this.count());
+            this._data.splice(position, 0, element);
+        }
+        return success;
+    }
+
+    flop() {
+        var ret = undefined;
+        if (this.count() > 0) {
+            var position = this._randomInt(this.count() - 1);
+            ret = this._data[position];
+            var shouldRemove = this._randomInt(1) > 0;
+            if (shouldRemove) {
+                this._data.splice(position, 1);
+            }
+        }
+        return ret;
+    }
+}
+
+var ff = new FlippyFlop();
+ff.flip(1)
+console.log(ff.count());
+ff.flip(2)
+console.log(ff.count());
+ff.flip(3)
+console.log(ff.count());
+ff.flip(4)
+console.log(ff.count());
+ff.flop();
+console.log(ff.count());
+ff.flop();
+console.log(ff.count());
